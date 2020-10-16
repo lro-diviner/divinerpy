c364cc327afa65256c2fc4e00d88197e5f083ea0
K.-Michael Aye <aye@ess.ucla.edu>
2020-10-16 00:59:19 -0700

    Bump version: 0.2.0 → 0.2.1

--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     test_suite="tests",
     tests_require=["pytest"],
     url="https://github.com/michaelaye/divinerpy",
-    version="0.2.0",
+    version="0.2.1",
     zip_safe=False,
 )
