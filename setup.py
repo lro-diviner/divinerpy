5a3a91fbfdb4ce1b73b28eff540acc902996fe6a
K.-Michael Aye <aye@ess.ucla.edu>
2020-10-17 00:28:04 -0700

    Bump version: 0.2.2 → 0.3.0

--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     test_suite="tests",
     tests_require=["pytest"],
     url="https://github.com/michaelaye/divinerpy",
-    version="0.2.2",
+    version="0.3.0",
     zip_safe=False,
 )
--- src/divinerpy/__init__.py
+++ src/divinerpy/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Michael Aye"""
 __email__ = "kmichael.aye@gmail.com"
-__version__ = "0.2.2"
+__version__ = "0.3.0"
